3315e86856b1b8e132ebeadea989fcd269b37118
DilinyerCR <dilinyerecr@mail.com>
2024-02-17 20:02:38 -0400

    drag and drop done

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,36 +1,81 @@
 import style from "./TodoItem.module.css";
 import CloseIcon from '../../assets/icon-cross.svg';
 import CheckIcon from '../../assets/icon-check.svg'
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const TodoItem = ({ task, handleCloseTask, handleCompleted, all }) => {
 
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition
+    } = useSortable({
+        id : task.id
+    })
+
+    const dndStyles = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    }
+
     return (
-        <div className={style.Items} key={task.id}>
-            <div className={style.div1}>
-                <div className={style.BackButton}
+        <div className={style.Main}>
+            
+            <div className={style.CheckButtonMainContainer}>
+              <div className={style.BackButton}>
+                <button
+                  className={style.Circle}
+                  onClick={() => handleCompleted(task.id)}
+                  style={{
+                    background: task.completed
+                      ? `linear-gradient(130deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))`
+                      : "var(--Very-Dark-Desaturated-Blue)",
+                  }}
                 >
-                    <button className={style.Circle} 
-                    onClick={()=>handleCompleted(task.id)}
-                    style={{
-                        background: task.completed
-                        ? `linear-gradient(130deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))`
-                        : 'var(--Very-Dark-Desaturated-Blue)'
-                    }}
-                    >
-                        {task.completed ? <img src={CheckIcon} alt="CheckIconON"/> : <img src='' alt=""/>}
-                    </button> 
-                </div>
-                <p 
-                style={{textDecoration: task.completed ? 'line-through' : 'none', 
-                color: task.completed ? 'var(--Very-Dark-Grayish-Blue-1)' : 'var(--Light-Grayish-Blue)'}}>{task.text}</p>
+                  {task.completed ? (
+                    <img src={CheckIcon} alt="CheckIconON" />
+                  ) : (
+                    <img src="" alt="" />
+                  )}
+                </button>
+              </div>
+            </div>
+            
+            <div
+              style={dndStyles}
+              ref={setNodeRef}
+              {...attributes}
+              {...listeners}
+              className={style.Items}
+              key={task.id}
+            >
+              <div className={style.div1}>
+                <p
+                  style={{
+                    textDecoration: task.completed ? "line-through" : "none",
+                    color: task.completed
+                      ? "var(--Very-Dark-Grayish-Blue-1)"
+                      : "var(--Light-Grayish-Blue)",
+                  }}
+                >
+                  {task.text}
+                </p>
+              </div>
             </div>
 
             <div>
-                <button className={style.CloseIcon} onClick={()=>handleCloseTask(task.id)} 
-                style={{display: all ? 'block' : 'none'}}>
-                    <img src={CloseIcon} alt="CloseIcon" />
-                </button>
+              <button
+                className={style.CloseIcon}
+                onClick={() => handleCloseTask(task.id)}
+                style={{ display: all ? "block" : "none" }}
+              >
+                <img src={CloseIcon} alt="CloseIcon" />
+              </button>
             </div>
+
         </div>
     );
 };
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,41 +1,37 @@
-import style from './TodoList.module.css'
-import { useState } from 'react'
-import TodoItem from '../TodoItem/TodoItem'
-
+import style from './TodoList.module.css';
+import { useState } from 'react';
+import TodoItem from '../TodoItem/TodoItem';
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable';
 
 const TodoList = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [allTasks, setAllTasks] = useState([]);
+  const [itemLeft, setItemLeft] = useState(0);
 
-  const [inputValue, setInputValue] = useState("")
-
-  const [tasks, setTasks] = useState([])
-
-  const [allTasks, setAllTasks] = useState([])
-
-  const [itemLeft, setItemLeft] = useState(0)
-
-  const [activeButton, setActiveButton] = useState(false)
-  const [completedButton, setCompletedButton] = useState(false)
-  const [all, setAll] = useState(true)
-
+  const [activeButton, setActiveButton] = useState(false);
+  const [completedButton, setCompletedButton] = useState(false);
+  const [all, setAll] = useState(true);
 
   // ? ===Funciones===
   const handleInputChange = (event) => {
-    setInputValue(event.target.value)
-  }
-    // Agregar tareas
+    setInputValue(event.target.value);
+  };
+  // Agregar tareas
   const handleAddTask = (event) => {
     event.preventDefault();
-  
+
     if (inputValue.trim()) {
       const newTask = {
         text: inputValue,
         id: Date.now(),
-        completed: false
+        completed: false,
       };
       setAllTasks([...allTasks, newTask]);
       setTasks([...allTasks, newTask]);
       setInputValue("");
-      setItemLeft(itemLeft + 1)
+      setItemLeft(itemLeft + 1);
     }
   };
 
@@ -51,17 +47,19 @@ const TodoList = () => {
     setAllTasks(updatedTasks);
     setTasks(updatedTasks);
   };
-  
+
   // Marcar tareas como completadas
   const handleCompleted = (id) => {
     const taskToUpdate = tasks.find((task) => task.id === id);
 
     const updatedTask = { ...taskToUpdate, completed: !taskToUpdate.completed };
 
-    const updatedTasks = tasks.map((task) => (task.id === id ? updatedTask : task));
-  
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? updatedTask : task
+    );
+
     const changeInItemLeft = updatedTask.completed ? -1 : 1;
-  
+
     setItemLeft((prevItemLeft) => prevItemLeft + changeInItemLeft);
     setAllTasks(updatedTasks);
     setTasks(updatedTasks);
@@ -70,22 +68,22 @@ const TodoList = () => {
   const filterActiveTasks = () => {
     const activeTasks = allTasks.filter((task) => !task.completed);
     setTasks(activeTasks);
-    if(!activeButton ) {
-      setActiveButton(true)
-      setCompletedButton(false)
-      setAll(false)
-    } 
-  }
+    if (!activeButton) {
+      setActiveButton(true);
+      setCompletedButton(false);
+      setAll(false);
+    }
+  };
 
   const filterCompletedTasks = () => {
     const completedTasks = allTasks.filter((task) => task.completed === true);
     setTasks(completedTasks);
-    if(!completedButton) {
+    if (!completedButton) {
       setCompletedButton(true);
-      setActiveButton(false)
-      setAll(false)
+      setActiveButton(false);
+      setAll(false);
     }
-  }
+  };
 
   const clearCompletedTasks = () => {
     const clearCompleted = allTasks.filter((task) => !task.completed);
@@ -93,20 +91,28 @@ const TodoList = () => {
 
     const activeTasks = tasks.filter((task) => !task.completed);
     setTasks(activeTasks);
+  };
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    setTasks((tasks) => {
+      const oldIndex = tasks.findIndex((task) => task.id === active.id)
+
+      const newIndex = tasks.findIndex((task) => task.id === over.id)
+      
+      return arrayMove(tasks, oldIndex, newIndex)
+    })
   }
 
-  const asd = () => {
-    const clearCompleted = allTasks.filter((task) => !task.completed);
-    setAllTasks(clearCompleted);
-    // Modificado aquí para mantener las tareas activas en la lista de tareas mostradas
-    const activeTasks = tasks.filter((task) => !task.completed);
-    setTasks(activeTasks);
-  }
+  // ! ====== Renderizado ======
+  return (
 
+    <DndContext 
+    collisionDetection={closestCenter} 
+    onDragEnd={handleDragEnd}
+    >
 
-  
-    // ! ====== Renderizado ======
-    return (
       <div className={style.TodoListMainContainer}>
         <div className={style.TodoList}>
           <form>
@@ -116,8 +122,8 @@ const TodoList = () => {
               placeholder="Create a new todo..."
               onChange={handleInputChange}
               onKeyDown={(event) => {
-                if(event.key === 'Enter') {
-                  handleAddTask(event)
+                if (event.key === "Enter") {
+                  handleAddTask(event);
                 }
               }}
               value={inputValue}
@@ -126,50 +132,171 @@ const TodoList = () => {
 
           <div className={style.ListItemsContainer}>
 
-            {tasks.map((task) => (
-              <TodoItem 
-              key={task.id}
-              task={task}
-              handleCloseTask={handleCloseTask}
-              handleCompleted={handleCompleted}
-              all={all}
-              />
-            ))}
-
+            <SortableContext
+            items={tasks} 
+            strategy={verticalListSortingStrategy}
+            >
+              {tasks.map((task) => (
+                <TodoItem
+                  key={task.id}
+                  task={task}
+                  handleCloseTask={handleCloseTask}
+                  handleCompleted={handleCompleted}
+                  all={all}
+                />
+              ))}
+            </SortableContext>
 
             <div className={style.ActionsContainerOne}>
               <p>{itemLeft} items left</p>
-                <div className={style.DektopOptionscontainer}>
-                  <button onClick={() => { setTasks(allTasks), setAll(true), setCompletedButton(false), setActiveButton(false) }} 
-                  style={{color: all ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>All</button>
+              <div className={style.DektopOptionscontainer}>
+                <button
+                  className={style.buttonall}
+                  onClick={() => {
+                    setTasks(allTasks),
+                      setAll(true),
+                      setCompletedButton(false),
+                      setActiveButton(false);
+                  }}
+                  style={{
+                    color: all
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.4s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!all) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = all ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  All
+                </button>
+                
+                <button
+                  onClick={filterActiveTasks}
+                  style={{
+                    color: activeButton
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.4s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!activeButton) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = activeButton ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  Active
+                </button>
 
-                  <button onClick={filterActiveTasks}
-                  style={{color: activeButton ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>Active</button>
+                <button
+                  onClick={filterCompletedTasks}
+                  style={{
+                    color: completedButton
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.3s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!completedButton) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = completedButton ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  Completed
+                </button>
+              </div>
 
-                  <button onClick={filterCompletedTasks} 
-                  style={{color: completedButton ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>Completed</button>
-                </div>
               <button onClick={clearCompletedTasks}>Clear Completed</button>
 
-
             </div>
+
+
           </div>
 
           <div className={style.ActionsContainerTwo}>
-            <button onClick={() => { setTasks(allTasks), setAll(true), setCompletedButton(false), setActiveButton(false) }} 
-            style={{color: all ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>All</button>
+          <button
+                  className={style.buttonall}
+                  onClick={() => {
+                    setTasks(allTasks),
+                      setAll(true),
+                      setCompletedButton(false),
+                      setActiveButton(false);
+                  }}
+                  style={{
+                    color: all
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.4s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!all) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = all ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  All
+                </button>
+                
+                <button
+                  onClick={filterActiveTasks}
+                  style={{
+                    color: activeButton
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.4s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!activeButton) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = activeButton ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  Active
+                </button>
 
-            <button onClick={filterActiveTasks}
-            style={{color: activeButton ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>Active</button>
-
-            <button onClick={filterCompletedTasks} 
-            style={{color: completedButton ? 'var(--Bright-Blue)' : 'var(--Dark-Grayish-Blue)'}}>Completed</button>
+                <button
+                  onClick={filterCompletedTasks}
+                  style={{
+                    color: completedButton
+                      ? "var(--Bright-Blue)"
+                      : "var(--Dark-Grayish-Blue)",
+                      transition: "color 0.3s"
+                  }}
+                  onMouseEnter={(event) => {
+                    if (!completedButton) {
+                        event.target.style.color = "var(--Very-Light-Grayish-Blue)";
+                    }
+                  }}
+                  onMouseLeave={(event) => {
+                    event.target.style.color = completedButton ? "var(--Bright-Blue)" : "var(--Dark-Grayish-Blue)";
+                }}
+                >
+                  Completed
+                </button>
           </div>
-
           <p className={style.DragAndDrop}>Drag and drop to reorder list</p>
         </div>
       </div>
-    );
-}
+    </DndContext>
 
-export default TodoList
+  );
+};
+
+export default TodoList;
